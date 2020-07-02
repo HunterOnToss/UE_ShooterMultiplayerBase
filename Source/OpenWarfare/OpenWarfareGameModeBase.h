367d151e9a43331d41b98b8198f5f5bd2f889da0
StanislavKablukov <stanislav_kablukov@rhinoda.com>
2020-07-03 05:23:31 +0700

    Setup Player

---
+++ Source/OpenWarfare/OpenWarfareGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenWarfareGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWARFARE_API AOpenWarfareGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
